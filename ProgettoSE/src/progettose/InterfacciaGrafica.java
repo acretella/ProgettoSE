e5335ef6762a4c3bdefbff904767d5e3eafe2481
Daniele <d.consalvo@studenti.unisa.it>
2020-11-23 18:59:58 +0100

    Aggiunta interfaccia grafica AGGIUNTA MODALITA' CREAZIONE ATTIVITA'

--- ProgettoSE/src/progettose/InterfacciaGrafica.java
+++ ProgettoSE/src/progettose/InterfacciaGrafica.java
@@ -5,6 +5,8 @@
  */
 package progettose;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Daniele
@@ -27,21 +29,223 @@ public class InterfacciaGrafica extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        GestioneAttività = new javax.swing.JFrame();
+        buttonCreaAttività = new javax.swing.JButton();
+        creazioneAttività = new javax.swing.JFrame();
+        tendinaAttività = new javax.swing.JComboBox<>();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        jLabel7 = new javax.swing.JLabel();
+        jLabel8 = new javax.swing.JLabel();
+        jLabel9 = new javax.swing.JLabel();
+        jLabel10 = new javax.swing.JLabel();
+        jLabel11 = new javax.swing.JLabel();
+        fieldID = new javax.swing.JTextField();
+        fieldFactorySite = new javax.swing.JTextField();
+        fieldArea = new javax.swing.JTextField();
+        fieldTypology = new javax.swing.JTextField();
+        fieldTime = new javax.swing.JTextField();
+        fieldWeek = new javax.swing.JTextField();
+        jComboBox1 = new javax.swing.JComboBox<>();
+        fieldMateriale = new javax.swing.JTextField();
+        buttonAggiungiMateriale = new javax.swing.JButton();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jTextArea1 = new javax.swing.JTextArea();
+        jScrollPane3 = new javax.swing.JScrollPane();
+        jTextArea3 = new javax.swing.JTextArea();
+        buttonCrea = new javax.swing.JButton();
+        jScrollPane5 = new javax.swing.JScrollPane();
+        jTextArea4 = new javax.swing.JTextArea();
+        buttonGestManut = new javax.swing.JButton();
+
+        GestioneAttività.setTitle("MENU' GESTIONE ATTIVITA'");
+        GestioneAttività.setMinimumSize(new java.awt.Dimension(600, 600));
+
+        buttonCreaAttività.setText("Crea attività");
+        buttonCreaAttività.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                buttonCreaAttivitàActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout GestioneAttivitàLayout = new javax.swing.GroupLayout(GestioneAttività.getContentPane());
+        GestioneAttività.getContentPane().setLayout(GestioneAttivitàLayout);
+        GestioneAttivitàLayout.setHorizontalGroup(
+            GestioneAttivitàLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(GestioneAttivitàLayout.createSequentialGroup()
+                .addGap(51, 51, 51)
+                .addComponent(buttonCreaAttività)
+                .addContainerGap(890, Short.MAX_VALUE))
+        );
+        GestioneAttivitàLayout.setVerticalGroup(
+            GestioneAttivitàLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(GestioneAttivitàLayout.createSequentialGroup()
+                .addGap(31, 31, 31)
+                .addComponent(buttonCreaAttività)
+                .addContainerGap(389, Short.MAX_VALUE))
+        );
+
+        creazioneAttività.setMinimumSize(new java.awt.Dimension(1100, 500));
+        creazioneAttività.getContentPane().setLayout(null);
+
+        tendinaAttività.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Planned", "EWO", "Extra" }));
+        tendinaAttività.setToolTipText("");
+        creazioneAttività.getContentPane().add(tendinaAttività);
+        tendinaAttività.setBounds(164, 31, 89, 20);
+
+        jLabel1.setText("Tipo di attività da creare");
+        creazioneAttività.getContentPane().add(jLabel1);
+        jLabel1.setBounds(10, 34, 144, 14);
+
+        jLabel2.setText("Activity ID");
+        creazioneAttività.getContentPane().add(jLabel2);
+        jLabel2.setBounds(10, 90, 70, 14);
+
+        jLabel3.setText("Factory site");
+        creazioneAttività.getContentPane().add(jLabel3);
+        jLabel3.setBounds(10, 130, 80, 14);
+
+        jLabel4.setText("Area");
+        creazioneAttività.getContentPane().add(jLabel4);
+        jLabel4.setBounds(10, 170, 60, 14);
+
+        jLabel5.setText("Typology");
+        creazioneAttività.getContentPane().add(jLabel5);
+        jLabel5.setBounds(10, 210, 70, 14);
+
+        jLabel6.setText("Activity Description");
+        creazioneAttività.getContentPane().add(jLabel6);
+        jLabel6.setBounds(500, 40, 140, 14);
+
+        jLabel7.setText("Time");
+        creazioneAttività.getContentPane().add(jLabel7);
+        jLabel7.setBounds(10, 260, 50, 14);
+
+        jLabel8.setText("Interruptible");
+        creazioneAttività.getContentPane().add(jLabel8);
+        jLabel8.setBounds(10, 340, 80, 20);
+
+        jLabel9.setText("Materials");
+        creazioneAttività.getContentPane().add(jLabel9);
+        jLabel9.setBounds(10, 390, 43, 20);
+
+        jLabel10.setText("Week");
+        creazioneAttività.getContentPane().add(jLabel10);
+        jLabel10.setBounds(10, 290, 50, 14);
+
+        jLabel11.setText("Workspace notes");
+        creazioneAttività.getContentPane().add(jLabel11);
+        jLabel11.setBounds(820, 40, 140, 14);
+        creazioneAttività.getContentPane().add(fieldID);
+        fieldID.setBounds(90, 80, 90, 30);
+        creazioneAttività.getContentPane().add(fieldFactorySite);
+        fieldFactorySite.setBounds(90, 120, 90, 30);
+        creazioneAttività.getContentPane().add(fieldArea);
+        fieldArea.setBounds(90, 160, 90, 30);
+        creazioneAttività.getContentPane().add(fieldTypology);
+        fieldTypology.setBounds(90, 200, 90, 30);
+
+        fieldTime.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                fieldTimeActionPerformed(evt);
+            }
+        });
+        creazioneAttività.getContentPane().add(fieldTime);
+        fieldTime.setBounds(90, 250, 90, 30);
+        creazioneAttività.getContentPane().add(fieldWeek);
+        fieldWeek.setBounds(90, 290, 90, 30);
+
+        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Si", "No", " " }));
+        creazioneAttività.getContentPane().add(jComboBox1);
+        jComboBox1.setBounds(90, 340, 90, 30);
+        creazioneAttività.getContentPane().add(fieldMateriale);
+        fieldMateriale.setBounds(90, 390, 90, 30);
+
+        buttonAggiungiMateriale.setText("Aggiungi");
+        creazioneAttività.getContentPane().add(buttonAggiungiMateriale);
+        buttonAggiungiMateriale.setBounds(200, 390, 82, 30);
+
+        jTextArea1.setColumns(20);
+        jTextArea1.setRows(5);
+        jScrollPane2.setViewportView(jTextArea1);
+
+        creazioneAttività.getContentPane().add(jScrollPane2);
+        jScrollPane2.setBounds(740, 70, 250, 140);
+
+        jTextArea3.setEditable(false);
+        jTextArea3.setColumns(20);
+        jTextArea3.setRows(5);
+        jScrollPane3.setViewportView(jTextArea3);
+
+        creazioneAttività.getContentPane().add(jScrollPane3);
+        jScrollPane3.setBounds(320, 310, 250, 140);
+
+        buttonCrea.setText("CREA ATTIVITA'");
+        creazioneAttività.getContentPane().add(buttonCrea);
+        buttonCrea.setBounds(750, 350, 250, 81);
+
+        jTextArea4.setColumns(20);
+        jTextArea4.setRows(5);
+        jScrollPane5.setViewportView(jTextArea4);
+
+        creazioneAttività.getContentPane().add(jScrollPane5);
+        jScrollPane5.setBounds(425, 69, 250, 140);
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("MENU' PRINCIPALE");
+
+        buttonGestManut.setText("Gestione Manutenzione");
+        buttonGestManut.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                buttonGestManutActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(140, 140, 140)
+                .addComponent(buttonGestManut)
+                .addContainerGap(132, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(54, 54, 54)
+                .addComponent(buttonGestManut)
+                .addContainerGap(223, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void buttonGestManutActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonGestManutActionPerformed
+
+      GestioneAttività.setVisible(true);
+
+
+    
+    }//GEN-LAST:event_buttonGestManutActionPerformed
+
+    private void buttonCreaAttivitàActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonCreaAttivitàActionPerformed
+       creazioneAttività.setVisible(true);
+    //*String[] opzioni ={"Planned","EWO","Extra"};
+   //* JOptionPane.showOptionDialog(null, "Scegli il tipo di attività da creare","Selezione tipo di attività", WIDTH, HEIGHT, null, opzioni, EXIT_ON_CLOSE);
+   //* creazioneAttività.setVisible(true);
+    
+
+        // TODO add your handling code here:
+    }//GEN-LAST:event_buttonCreaAttivitàActionPerformed
+
+    private void fieldTimeActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_fieldTimeActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_fieldTimeActionPerformed
 
     /**
      * @param args the command line arguments
@@ -79,5 +283,37 @@ public class InterfacciaGrafica extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JFrame GestioneAttività;
+    private javax.swing.JButton buttonAggiungiMateriale;
+    private javax.swing.JButton buttonCrea;
+    private javax.swing.JButton buttonCreaAttività;
+    private javax.swing.JButton buttonGestManut;
+    private javax.swing.JFrame creazioneAttività;
+    private javax.swing.JTextField fieldArea;
+    private javax.swing.JTextField fieldFactorySite;
+    private javax.swing.JTextField fieldID;
+    private javax.swing.JTextField fieldMateriale;
+    private javax.swing.JTextField fieldTime;
+    private javax.swing.JTextField fieldTypology;
+    private javax.swing.JTextField fieldWeek;
+    private javax.swing.JComboBox<String> jComboBox1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel10;
+    private javax.swing.JLabel jLabel11;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
+    private javax.swing.JLabel jLabel9;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JScrollPane jScrollPane3;
+    private javax.swing.JScrollPane jScrollPane5;
+    private javax.swing.JTextArea jTextArea1;
+    private javax.swing.JTextArea jTextArea3;
+    private javax.swing.JTextArea jTextArea4;
+    private javax.swing.JComboBox<String> tendinaAttività;
     // End of variables declaration//GEN-END:variables
 }
