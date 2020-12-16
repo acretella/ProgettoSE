41f6e5e9121cc2dea9009d979562eeba52e87319
Rossella <73583618+Rossella7@users.noreply.github.com>
2020-12-16 19:31:31 +0100

    add comment

--- ProgettoSE/src/progettose/Admin.java
+++ ProgettoSE/src/progettose/Admin.java
@@ -6,9 +6,6 @@
 package progettose;
 
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 /**
  *
  * @author Rossella
@@ -19,47 +16,70 @@ public class Admin extends User {
         super(username, password);
     }
     
+    /**
+     * Inserisce la competenza passata come parametro nel DB
+     * @param competence nome della competenza da inserire
+     * @return true quando l'inserimento viene eseguito correttamente, false quando la competenza è già presente nel DB o ci sono SQLException
+     */
     public boolean createCompetence(String competence){
         try{
-            Statement stm = super.getConnection().createStatement();
             String query = "insert into Competence(skill) values ('" + competence + "');";
-            stm.executeUpdate(query);
+            super.getConnection().createStatement().executeUpdate(query);
             return true;
         } catch (SQLException ex){
             return false;
         }
     }
     
+    /**
+     * Elimina la competenza passata come parametro dal DB
+     * @param competence nome della competenza da cancellare
+     * @return true quando la cancellazione viene eseguita correttamente, false la competenza da cancellare non è presente nel DB o ci sono SQLException
+     */
     public boolean deleteCompetence(String competence){
         try{
-            Statement stm = super.getConnection().createStatement();
             String query = "delete from Competence where skill = '" + competence +"';";
-            return stm.executeUpdate(query) == 1;
+            return super.getConnection().createStatement().executeUpdate(query) == 1;
         } catch(SQLException ex){
             return false;
         }
     }
     
+    /**
+     * Effettua una update sulla tabella competence
+     * @param oldCompetence nome della competenza da modificare
+     * @param newCompetence nuovo nome della competenza
+     * @return true quando la modifica viene eseguita correttamente, false quando la competenza da modificare non è presente nel DB o ci sono SQLException
+     */
     public boolean modifyCompetence(String oldCompetence, String newCompetence){
         try{
-            Statement stm = super.getConnection().createStatement();
             String query = "update Competence set skill = '" + newCompetence + "' where skill = '" + oldCompetence + "';";
-            return stm.executeUpdate(query) == 1;
+            return super.getConnection().createStatement().executeUpdate(query) == 1;
         }catch (SQLException ex){
             return false;
         }
     }
     
+    /**
+     * Cancella il sito passato come parametro dal DB
+     * @param site nome del sito da cancellare
+     * @return true quando la cancellazione viene effettuata correttamente, false quando il sito da cancellare non è presente o ci sono SQLException 
+     */
     public boolean deleteSite(Site site) {
         try {
-            Statement stm = super.getConnection().createStatement();
             String query = "delete from Site where (factory_site, area) = ('" + site.getFactorySite() + "', '" + site.getArea() + "')";
-            return stm.executeUpdate(query) == 1;
+            return super.getConnection().createStatement().executeUpdate(query) == 1;
         } catch (SQLException ex) {
             System.out.println(ex);
             return false;
         }
     }
+    
+    /**
+     * Inserisce il sito passato come parametro nel DB
+     * @param s nome del sito da inserire
+     * @return true quando l'inserimetno viene eseguito correttamente, false quando il sito è già presente nel DB o ci sono SQLException
+     */
     public boolean createSite(Site s){
         try {
             String query = "insert into Site(factory_site,area) values('" + s.getFactorySite() +"','"+s.getArea()+"');";
@@ -70,6 +90,12 @@ public class Admin extends User {
         }
     }
     
+    /**
+     * Effettua una update sulla tabella Site
+     * @param old sito da modificare
+     * @param nw sito aggiornato
+     * @return true quando la modifica viene effettuata correttamente, false quando nel DB non è presente il sito da modificare o ci sono SQLException
+     */
     public boolean modifySite(Site old,Site nw){
         try {
             String query = "update Site set factory_site = '" +nw.getFactorySite() +"',"
--- ProgettoSE/src/progettose/User.java
+++ ProgettoSE/src/progettose/User.java
@@ -7,7 +7,6 @@ package progettose;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.Statement;
 import java.sql.SQLException;
 import java.sql.ResultSet;
 import java.util.ArrayList;
@@ -33,41 +32,60 @@ public class User {
         return connection;
     }
 
+    /**
+     * Inserisce il materiale passato come parametro nel DB 
+     * @param material nome del materiale da inserire
+     * @return true quando il materiale viene inserito correttamente,
+     *         false quando nel DB esiste un materiale con lo stesso nome o se ci sono delle SQLException
+     */
     public boolean createMaterial(String material){
         try{
-            Statement stm = connection.createStatement();
             String query = "insert into Material(materialName) values ('" + material +"');";
-            stm.executeUpdate(query);
+            connection.createStatement().executeUpdate(query);
             return true;
         } catch(SQLException ex){
             return false;
         }
     }
     
+    /**
+     * Cancella il materiale passato come parametro dal DB
+     * @param material nome del materiale da cancellare
+     * @return true quando il materiale è stato cancellato correttamente,
+     *         false quando il materiale non è presente nel DB o se ci sono delle SQLException
+     */
     public boolean deleteMaterial(String material){
         try{
-            Statement stm = connection.createStatement();
             String query = "delete from Material where materialName= '" + material + "';";
-            return stm.executeUpdate(query) == 1; //controllo il valore restituito per stabilire se la execute ha effettuato la delete
+            return connection.createStatement().executeUpdate(query) == 1; //controllo il valore restituito per stabilire se la execute ha effettuato la delete
         } catch (SQLException ex){
             return false;
         }
     }
     
+    /**
+     * Effettua una update sulla tabella material
+     * @param oldMaterial nome del materiale da modificare
+     * @param newMaterial nuovo nome del materiale
+     * @return true quando la modifica viene effettuata correttamente,
+     *         false quando il materiale da modificare non è presente nel DB o ci sono delle SQLException
+     */
     public boolean modifyMaterial(String oldMaterial, String newMaterial){
         try{
-            Statement stm = connection.createStatement();
             String query = "update Material set materialName = '" + newMaterial + "' where materialName = '" + oldMaterial +"';";
-            return stm.executeUpdate(query) == 1;//controllo il valore restituito per statbiilire se la exectue ha effettuato la update
+            return connection.createStatement().executeUpdate(query) == 1;//controllo il valore restituito per statbiilire se la exectue ha effettuato la update
         } catch(SQLException ex){
             return false;
         }
     }
     
+    /**
+     * Prende dal DB i materiali presenti nella relavita tabella
+     * @return ArrayList di stringhe contenente i nomi dei materiali
+     */
     public List<String> getAllMaterials(){
         try{
-            Statement stm = connection.createStatement();
-            ResultSet rst = stm.executeQuery("select * from Material;");
+            ResultSet rst = connection.createStatement().executeQuery("select * from Material;");
             List<String> materials = new ArrayList<>();
             while (rst.next()){
                 materials.add(rst.getString("materialName"));
@@ -78,10 +96,13 @@ public class User {
         }
     }
     
+    /**
+     * Prende dal DB le skill presenti nella tabella Competence
+     * @return ArrayList di Stringhe contenenti i nomi delle skill
+     */
     public List<String> getAllSkills(){
         try{
-            Statement stm = connection.createStatement();
-            ResultSet rst = stm.executeQuery("select * from Competence");
+            ResultSet rst = connection.createStatement().executeQuery("select * from Competence");
             List<String> skills = new ArrayList<>();
             while(rst.next()){
                 skills.add(rst.getString("skill"));
@@ -92,10 +113,13 @@ public class User {
         }
     }
     
+    /**
+     * Prende dal DB i siti presenti nella tabella Site
+     * @return ArrayList di Site 
+     */
     public List<Site> getAllSites() {
         try {
-            Statement stm = connection.createStatement();
-            ResultSet rst = stm.executeQuery("select * from Site");
+            ResultSet rst = connection.createStatement().executeQuery("select * from Site");
             List<Site> sites = new ArrayList<>();
             while (rst.next()) {
                 Site s = new Site(rst.getString("factory_site"), rst.getString("area"));
